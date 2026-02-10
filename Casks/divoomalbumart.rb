2be1e5800965d3774b6c8049302aea94c8aa4112
Tim Broddin <tim@broddin.be>
2026-02-10 13:21:57 +0100

    Update SHA256 for v1.0.0 release

--- Casks/divoomalbumart.rb
+++ Casks/divoomalbumart.rb
@@ -1,6 +1,6 @@
 cask "divoomalbumart" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_SHA256_OF_DMG"
+  sha256 "0ca6430626ba815dd0ae8197d797ce3ed536537e6cf5ffb39640e786e9c1c5b7"
 
   url "https://github.com/TimBroddin/DivoomAlbumArt/releases/download/v#{version}/DivoomAlbumArt.dmg"
   name "DivoomAlbumArt"
